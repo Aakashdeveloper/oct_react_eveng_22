53dd89d4f980f4902243c7e4f25216283e4a7cad
aakashdeveloper <ahanda205@gmail.com>
2022-10-20 21:57:09 +0530

    classcomponent

---
+++ firstapp/src/component/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const App = () => {
+    return(
+        <>
+            <Header/>
+            <h1>Hiii From React</h1>
+            <h2>Test Component</h2>
+            <Footer/>
+        </>
+    )
+}
+
+export default App;
--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,38 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './Header'
-
-const App = () => {
-    return(
-        <div>
-            <Header/>
-            <h1>Hiii From React</h1>
-            <h2>Test Component</h2>
-        </div>
-    )
-}
-
+import App from './component/App'
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<App/>)
-
-
-
-//import React from 'react';
-// var React = require('react');
-// var ReactDOM = require('react-dom/client')
-// //import ReactDOM from 'react-dom/client';
-
-// function App(){
-//     return(
-//         <h1>Hiii From React Es5</h1>
-//     )
-// }
-
-// // const App = () => {
-// //     return(
-// //         <h1>Hiii From React</h1>
-// //     )
-// // }
-
-// //ReactDOM.render(<App/>, document.getElementById('root'))
