187742fade68ffa01d6458e929da6807cd956269
aakashdeveloper <ahanda205@gmail.com>
2022-11-10 21:35:52 +0530

    filters

--- zomapp/src/component/listing/listingApi.js
+++ zomapp/src/component/listing/listingApi.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import axios from 'axios';
 import './listing.css';
 import ListingDisplay from './listingDisplay';
+import CuisineFilter from '../filters/cuisineFilter';
+import CostFilter from '../filters/costFilter';;
 
 const url = "https://zomatoajulypi.herokuapp.com/restaurant?mealtype_id="
 
@@ -15,12 +17,22 @@ class Listing extends Component {
         }
     }
 
+    setDataPerFilter = (data) => {
+        this.setState({restaurantList:data})
+    }
+
     render(){
         return(
             <>
                 <div className="row">
                     <div id="mainListing">
-                        <div id="filter"></div>
+                        <div id="filter">
+                            <center><h2>Filters</h2></center>
+                            <CuisineFilter mealId={this.props.match.params.mealId}
+                            restPerCuisine={(data)=>{this.setDataPerFilter(data)}}/>
+                            <CostFilter mealId={this.props.match.params.mealId}
+                            restPerCost={(data)=>{this.setDataPerFilter(data)}}/>
+                        </div>
                         <ListingDisplay listData={this.state.restaurantList}/>
                     </div>
                 </div>
